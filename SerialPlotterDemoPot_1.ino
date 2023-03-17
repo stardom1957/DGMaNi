86ff160797d2707c9859676fb3ed81bc84e8b4ee
Guy Nicol <105818788+GuyNicol@users.noreply.github.com>
2023-03-16 21:11:03 -0400

    Add files via upload
    Fichier Draw avec diagramme de circuit.
    Fichier texte avec programme .ino pour tester sur Arduino.

---
+++ SerialPlotterDemoPot_1.ino
@@ -0,0 +1,27 @@
+int pot,                  // pot contient la valeur actuelle du pot, ancien_pot
+    ancien_pot;           // ancien_pot contient la valeur précédente
+int sortieST;             // Valeur lue à la sortie de l'inverseur Schmitt-Trigger (ST)
+float valPot, valSortieST;// Utilisé pour convertir en voltage les valeurs lues
+int tolerance=10;         // Permet de réduire l'axe X du graphique affiché 50 = 1/2V approx.
+
+void setup() {
+  Serial.begin(9600);
+  pot = analogRead(A1);
+  ancien_pot = pot;
+}
+
+void loop() {
+  pot = analogRead(A1);
+  while ((pot >= ancien_pot+tolerance) or (pot <= ancien_pot-tolerance)) {  // Si changement
+    sortieST= analogRead(A2);
+    valPot = float(pot*5)/1024; // Conversion en volts pour l'affichage
+    valSortieST = float(sortieST*5)/1024; // Conversion en volts pour l'affichage
+
+    Serial.print("Pot:");
+    Serial.println(valPot);
+    Serial.print("SortieST:");
+    Serial.println(valSortieST);
+
+    ancien_pot = pot;
+  }  
+}
